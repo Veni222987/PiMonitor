2c8ed3101528939323d3597638ad2e1fad33c7e9
Veni <veni222987@gmail.com>
2024-04-27 02:00:35 +0800

    [feat] change gopsutil version

--- agent/go.mod
+++ agent/go.mod
@@ -2,10 +2,13 @@ module Agent
 
 go 1.22
 
-require github.com/shirou/gopsutil v3.21.11+incompatible
-
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shirou/gopsutil/v3 v3.24.3 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.13 // indirect
 	github.com/tklauser/numcpus v0.7.0 // indirect
--- agent/monitor/base_info.go
+++ agent/monitor/base_info.go
@@ -2,66 +2,30 @@ package monitor
 
 import (
 	"fmt"
-	"github.com/shirou/gopsutil/cpu"
-	"golang.org/x/sys/unix"
-	"runtime"
+	"github.com/shirou/gopsutil/v3/cpu"
 )
 
 type ComputerInfo struct {
 	CPU         CPUInfo
 	Memory      int      // 内存，单位为GB
-	NetworkCard []string //网卡名称，有需要可以获取网卡的MAC地址
-	OS          string   //操作系统
+	NetworkCard []string // 网卡名称，有需要可以获取网卡的MAC地址
+	OS          string   // 操作系统
 }
 
 type CPUInfo struct {
-	CPUName      string //CPU名称
-	LogicCore    int    // CPU核心数
-	PhysicalCore int    // CPU线程数
+	CPUName   string  // CPU名称
+	Core      int     // CPU核心数
+	Frequency float64 // 频率 MHZ
 }
 
 func GetCPUInfo() (*CPUInfo, error) {
-	// gopsutil无法实现全兼容，这里需要手动实现
-	switch os := runtime.GOOS; os {
-	case "windows":
-		return getDefaultCPUInfo()
-		// 在Windows系统下执行特定代码
-	case "darwin":
-		return getMacCPUInfo()
-	case "linux":
-		return getDefaultCPUInfo()
-	default:
-		return nil, fmt.Errorf("unsupported OS")
-	}
-}
-
-func getMacCPUInfo() (*CPUInfo, error) {
-	cpuName, err := unix.Sysctl("machdep.cpu.brand_string")
-	if err != nil {
-		return nil, fmt.Errorf("fail to get cpu name: %s", err)
-	}
-
-	logicCore, err := cpu.Counts(true)
-	if err != nil {
-		return nil, fmt.Errorf("fail to get cpu logic core: %s", err)
-	}
-
-	physicalCore, err := cpu.Counts(false)
-	if err != nil {
-		return nil, fmt.Errorf("fail to get cpu physical core: %s", err)
-	}
-
-	return &CPUInfo{CPUName: cpuName, LogicCore: logicCore, PhysicalCore: physicalCore}, nil
-}
-
-func getDefaultCPUInfo() (*CPUInfo, error) {
 	cpus, err := cpu.Info()
 	if err != nil {
-		return nil, fmt.Errorf("get cpu info err %s", err)
+		return nil, fmt.Errorf("get cpu info err: %s", err)
 	}
 	if len(cpus) == 0 {
 		return nil, fmt.Errorf("no cpu found")
 	}
-	return &CPUInfo{CPUName: cpus[0].ModelName, LogicCore: int(cpus[0].Cores), PhysicalCore: int(cpus[0].Cores * 2)}, nil
+	return &CPUInfo{CPUName: cpus[0].ModelName, Core: int(cpus[0].Cores), Frequency: cpus[0].Mhz}, nil
 
 }
