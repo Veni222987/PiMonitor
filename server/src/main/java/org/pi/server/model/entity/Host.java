d7ee2947b26c965d206c0d9eb41323b6be1823e4
hu1hu <3261801992@qq.com>
2024-05-19 23:54:39 +0800

    feat(server): 生成初始代码

---
+++ server/src/main/java/org/pi/server/model/entity/Host.java
@@ -0,0 +1,76 @@
+package org.pi.server.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+* <p>
+* 
+* </p>
+* @author hu1hu
+* @since 2024-05-19
+*/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("t_host")
+public class Host {
+
+    /**
+    * 主机自增id
+    */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+    * MAC地址
+    */
+    private String mac;
+
+    /**
+    * cpu信息 {cpu_name core frequency}
+    */
+    private String cpu;
+
+    /**
+    * 内存大小
+    */
+    private Integer memory;
+
+    /**
+    * 磁盘大小
+    */
+    private Integer disk;
+
+    /**
+    * 网卡信息
+    */
+    private String networkCard;
+
+    /**
+    * 操作系统
+    */
+    private String os;
+
+    /**
+    * 最后一次上报时间，用于监控存活情况
+    */
+    private LocalDateTime lastTime;
+
+    /**
+    * 状态：非监控  监控 异常
+    */
+    private String status;
+
+    /**
+    * 主机别名，默认为id
+    */
+    private String hostname;
+
+}
---
+++ server/src/main/java/org/pi/server/model/entity/User.java
@@ -0,0 +1,73 @@
+package org.pi.server.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+* <p>
+* 
+* </p>
+* @author hu1hu
+* @since 2024-05-19
+*/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("t_user")
+public class User {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+    * 用户名
+    */
+    private String username;
+
+    /**
+    * 头像，存储url 使用oss存储
+    */
+    private String avater;
+
+    /**
+    * 电话号码
+    */
+    private String phoneNumber;
+
+    /**
+    * 邮箱
+    */
+    private String email;
+
+    /**
+    * 密码
+    */
+    private String password;
+
+    /**
+    * 角色id
+    */
+    private String roleId;
+
+    /**
+    * 状态
+    */
+    private String status;
+
+    /**
+    * 最后登录时间
+    */
+    private LocalDateTime lastLoginTime;
+
+    /**
+    * 记录用户登录尝试次数，用于安全监控
+    */
+    private Byte loginAttempts;
+
+}
