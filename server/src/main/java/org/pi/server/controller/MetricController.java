b5b0e0583f3479316320030fb68aaa4d4ab11d24
hu1hu <3261801992@qq.com>
2024-05-22 22:35:07 +0800

    feat(server): 完成agent推送接口

--- server/src/main/java/org/pi/server/controller/MetricController.java
+++ server/src/main/java/org/pi/server/controller/MetricController.java
@@ -1,7 +1,11 @@
 package org.pi.server.controller;
 
+import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.pi.server.common.ResultUtils;
+import org.pi.server.model.dto.MetricInfo;
+import org.pi.server.repo.KafkaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +19,9 @@ import org.pi.server.common.Result;
 @RestController
 @RequestMapping("/v1/agents/{agentID}/services/info")
 public class MetricController {
+    @Autowired
+    private KafkaRepo kafkaRepo;
+
     @GetMapping
     public Result<Object> getMetricInfo(@PathVariable String agentID, @DateTimeFormat LocalDateTime startTime, @DateTimeFormat LocalDateTime endTime) {
         log.debug("agentID:{},startTime:{},endTime:{}", agentID, startTime, endTime);
@@ -24,10 +31,10 @@ public class MetricController {
     }
 
     @PostMapping
-    public Result<Object> postMetricInfo(@PathVariable String agentID, @DateTimeFormat LocalDateTime time, @RequestBody Map<String, Object> map) {
-        log.debug("agentID:{},time:{},performance:{}", agentID, time, map);
+    public Result<Object> postMetricInfo(@PathVariable String agentID, @DateTimeFormat LocalDateTime time, @RequestBody Map<String, Object> metric) {
+        log.debug("agentID:{},time:{},performance:{}", agentID, time, metric);
         //TODO: 这里调用Kafka生产者，生产一条消息
-
+        kafkaRepo.produce("service", JSONObject.toJSONString(new MetricInfo(agentID, time, metric)));
         return ResultUtils.success();
     }
     
