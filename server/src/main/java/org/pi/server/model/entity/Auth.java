af5992a2e8cdf32e249a56642d12611563a38ae6
hu1hu <3261801992@qq.com>
2024-05-28 01:37:05 +0800

    feat(server): 修改数据库表及其Mapper文件

---
+++ server/src/main/java/org/pi/server/model/entity/Auth.java
@@ -0,0 +1,27 @@
+package org.pi.server.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p>
+ *
+ * </p>
+ * @author hu1hu
+ * @since 2024-05-26
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("t_auth")
+public class Auth {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    private Long userId;
+    private String openId;
+    private String type;
+}
