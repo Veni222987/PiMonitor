5ca60f091c889f2705e2e8a77ac480e7d75f7eac
hu1hu <3261801992@qq.com>
2024-05-28 01:49:24 +0800

    feat(server): 添加拦截器

---
+++ server/src/main/java/org/pi/server/aop/AuthCodeInterceptor.java
@@ -0,0 +1,57 @@
+package org.pi.server.aop;
+
+import com.alibaba.fastjson.JSONObject;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.pi.server.common.Result;
+import org.pi.server.common.ResultCode;
+import org.pi.server.common.ResultUtils;
+import org.pi.server.utils.JwtUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Component
+public class AuthCodeInterceptor implements HandlerInterceptor {
+    @Override// 目标资源方法运行前运行，返回true:放行，返回false，不放行
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.获取请求的url
+        String url = request.getRequestURL().toString();
+
+        // 2.获取请求头中的令牌（Authorization）
+        String jwt = request.getHeader("Authorization");
+
+        // 3.判断令牌是否存在。
+        if(!StringUtils.hasLength(jwt)){
+            return true;
+        }
+
+        // 4.解析token,如果解析失败，返回结果
+        try{
+            Claims claims = JwtUtil.parseJWT(jwt);
+            claims.forEach(request::setAttribute);
+        }catch(Exception e){
+            Result<Object> error = ResultUtils.error(ResultCode.PARAMS_ERROR);
+            // 手动转换 对象--json ------> 阿里巴巴fastJSON
+            String result = JSONObject.toJSONString(error);
+            response.getWriter().write(result);
+            return false;
+        }
+        // 5.放行
+        return true;
+    }
+
+    @Override // 目标资源方法运行后运行
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override // 视图渲染完毕后运行，最后运行
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
