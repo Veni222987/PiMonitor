29f0b24f3e707f9e6cc37113e1d6bc8c373cf007
hu1hu <3261801992@qq.com>
2024-05-28 01:47:13 +0800

    feat(server): 添加返回类型

--- server/src/main/java/org/pi/server/common/ResultCode.java
+++ server/src/main/java/org/pi/server/common/ResultCode.java
@@ -16,6 +16,9 @@ public enum ResultCode {
     NOT_LOGIN_ERROR(40100, "未登录"),
     NO_AUTH_ERROR(40101, "无权限"),
     NOT_FOUND_ERROR(40400, "请求数据不存在"),
+    REQUEST_TOO_FREQUENT(40200, "请求次数过于频繁"),
+    VERIFY_CODE_ERROR(40201, "验证码错误"),
+    PASSWORD_ERROR(40200, "密码错误"),
     FORBIDDEN_ERROR(40300, "禁止访问"),
 
     SYSTEM_ERROR(50000, "系统内部异常"),
