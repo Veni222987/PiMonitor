ae7092340ff01c6b67d23bfec30604717138d7ec
hu1hu <3261801992@qq.com>
2024-06-04 18:21:25 +0800

    feat(server): 修改配置文件
    完善拦截器配置、yml添加jwt、logging 配置

--- server/src/main/java/org/pi/server/config/InterceptorConfig.java
+++ server/src/main/java/org/pi/server/config/InterceptorConfig.java
@@ -2,7 +2,7 @@ package org.pi.server.config;
 
 import org.pi.server.annotation.GetAttributeResolver;
 import org.pi.server.aop.AuthCodeInterceptor;
-import org.pi.server.aop.UserInterceptor;
+import org.pi.server.aop.CommonInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -14,17 +14,21 @@ import java.util.List;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Autowired
-    private UserInterceptor userInterceptor;
+    private CommonInterceptor commonInterceptor;
     @Autowired
     private AuthCodeInterceptor authCodeInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(userInterceptor)
+        registry.addInterceptor(commonInterceptor)
                 .addPathPatterns(
-                        "/v1/users/register",
-                        "/v1/users/resetPassword",
-                        "/v1/users/modify"
+                        "/v1/**"
+                ).excludePathPatterns(
+                        "/v1/common/aliyun/code", // 发送验证码
+                        "/v1/common/authCode",  // 验证验证码
+                        "/v1/users/login", // 登录
+                        "/v1/oauth/login/{type}", // 第三方登录
+                        "/v1/oauth/{type}/callback" // 第三方登录回调
                 );
         registry.addInterceptor(authCodeInterceptor).addPathPatterns("/v1/common/authCode");
     }
@@ -37,6 +41,4 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new GetAttributeResolver());
     }
-
-
 }
