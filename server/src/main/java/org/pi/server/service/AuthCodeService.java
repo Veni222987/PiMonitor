5c57d95721a8f215084ad826d28ac0c67892a02a
hu1hu <3261801992@qq.com>
2024-06-04 18:39:50 +0800

    feat(server): 修改验证码模块逻辑
    聚合登录和注册

--- server/src/main/java/org/pi/server/service/AuthCodeService.java
+++ server/src/main/java/org/pi/server/service/AuthCodeService.java
@@ -3,7 +3,8 @@ package org.pi.server.service;
 import java.util.Map;
 
 public interface AuthCodeService {
-    String loginAuthCode(Map<String,Object> map);
-    String registerAuthCode(Map<String,Object> map);
+    String loginOrRegisterAuthCode(Map<String,Object> map);
+
+    String resetPasswordAuthCode(Map<String,Object> map);
     String bind(String userID, Map<String, Object> requestMap);
 }
--- server/src/main/java/org/pi/server/service/impl/AuthCodeServiceImpl.java
+++ server/src/main/java/org/pi/server/service/impl/AuthCodeServiceImpl.java
@@ -1,13 +1,17 @@
 package org.pi.server.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import org.jetbrains.annotations.NotNull;
 import org.pi.server.model.entity.User;
 import org.pi.server.service.AuthCodeService;
 import org.pi.server.service.RedisService;
 import org.pi.server.service.UserService;
+import org.pi.server.utils.CodeUtils;
 import org.pi.server.utils.JwtUtils;
+import org.pi.server.utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,95 +24,105 @@ public class AuthCodeServiceImpl implements AuthCodeService {
     @Autowired
     private UserService userService;
 
+    /**
+     * 登录或注册验证码
+     * @param map
+     * @return String jwt：成功  “-1”： 验证码不存在  “-2”： 验证码错误  “-3”：系统错误
+     */
     @Override
-    public String loginAuthCode(Map<String, Object> map) {
-        if (map.containsKey("phoneNumber")) {
-            if (redisService.get(map.get("phoneNumber").toString()) == null) {
-                return "-1";
+    @Transactional
+    public String loginOrRegisterAuthCode(@NotNull Map<String, Object> map) {
+        String account = (String) map.get("account");
+        String code = redisService.get(account);
+
+        if (code == null) { // 验证码不存在
+            return "-1";
+        } else if (code.equals(map.get("code"))) { // 验证码正确
+            long userID;
+            if (account.contains("@")) { // 邮箱
+                if (!userService.exists("email", account)) { // 未注册
+                    User user = new User();
+                    user.setEmail(account);
+                    user.setPassword(PasswordUtils.encryptPassword("pim123456"));
+                    user.setUsername("pim_" + CodeUtils.generateVerifyCode(6));
+                    userService.insertUser(user);
+                }
+                userID = userService.getIDByEmail(account);
+            } else { // 手机号
+                if (!userService.exists("phone_number", account)) { // 未注册
+                    User user = new User();
+                    user.setPhoneNumber(account);
+                    user.setPassword(PasswordUtils.encryptPassword("pim123456"));
+                    user.setUsername("pim_" + CodeUtils.generateVerifyCode(6));
+                    userService.insertUser(user);
+                }
+                userID = userService.getIDByPhoneNumber(account);
             }
-            if (redisService.get(map.get("phoneNumber").toString()).equals(map.get("code"))) {
-                long userID = userService.getIDByPhoneNumber(map.get("phoneNumber").toString());
-                // 认证成功 下发jwt
-                Map<String, Object> claims = new HashMap<>();
-                claims.put("userID", userID);
-                return JwtUtils.generateJwt(claims, 30 * 60L); // 30分钟
-            }
-        } else if (map.containsKey("email")) {
-            if (redisService.get(map.get("email").toString()) == null) {
-                return "-1";
-            }
-            if (redisService.get(map.get("email").toString()).equals(map.get("code"))) {
-                long userID = userService.getIDByEmail(map.get("email").toString());
-                // 认证成功 下发jwt
-                Map<String, Object> claims = new HashMap<>();
-                claims.put("userID", userID);
-                return JwtUtils.generateJwt(claims, 30 * 60L); // 30分钟
-            }
+            // 认证成功 下发jwt
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("userID", userID + "");
+            return JwtUtils.generateJwt(claims);
+        } else { // 验证码错误
+            return "-2";
         }
-        return "-2";
     }
 
+    /**
+     * 重置密码验证码
+     * @param map
+     * @return String jwt：成功  “-1”： 验证码
+     */
     @Override
-    public String registerAuthCode(Map<String, Object> map) {
-        if (map.containsKey("phoneNumber")) {
-            if (redisService.get(map.get("phoneNumber").toString()) == null) {
-                return "-1";
+    public String resetPasswordAuthCode(Map<String, Object> map) {
+        String account = (String) map.get("account");
+        String code = redisService.get(account);
+
+        if (code == null) { // 验证码不存在
+            return "-1";
+        } else if (code.equals(map.get("code"))) { // 验证码正确
+            long userID;
+            Map<String, Object> claims = new HashMap<>();
+            if (account.contains("@")) { // 邮箱
+                claims.put("email", account);
+            } else { // 手机号
+                claims.put("phoneNumber", account);
             }
-            if (redisService.get(map.get("phoneNumber").toString()).equals(map.get("code"))) {
-                // 认证成功 下发jwt
-                Map<String, Object> claims = new HashMap<>();
-                claims.put("phoneNumber", map.get("phoneNumber"));
-                return JwtUtils.generateJwt(claims, 30 * 60L); // 30分钟
-            }
-        } else if (map.containsKey("email")) {
-            if (redisService.get(map.get("email").toString()) == null) {
-                return "-1";
-            }
-            if (redisService.get(map.get("email").toString()).equals(map.get("code"))) {
-                // 认证成功 下发jwt
-                Map<String, Object> claims = new HashMap<>();
-                claims.put("email", map.get("email"));
-                return JwtUtils.generateJwt(claims, 30 * 60L);
-            }
+            return JwtUtils.generateJwt(claims, 60 * 30L); // 30分钟
+        } else { // 验证码错误
+            return "-2";
         }
-        return "-2";
     }
 
+    /**
+     * 绑定手机号或邮箱
+     * @param userID
+     * @param map
+     * @return String jwt：成功  “-1”： 验证码不存在  “-2”： 验证码错误  “-3”：系统错误
+     */
     @Override
     public String bind(String userID, Map<String, Object> map) {
-        if (map.containsKey("phoneNumber")) {
-            if (redisService.get(map.get("phoneNumber").toString()) == null) {
-                return "-1";
+        String account = (String) map.get("account");
+        String code = redisService.get(account);
+
+        if (code == null) { // 验证码不存在
+            return "-1";
+        } else if (code.equals(map.get("code"))) { // 验证码正确
+            UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+            if (account.contains("@")) { // 绑定邮箱
+                updateWrapper.eq("id", userID).set("email", account);
+            } else { // 绑定手机号
+                updateWrapper.eq("id", userID).set("phone_number", account);
             }
-            if (redisService.get(map.get("phoneNumber").toString()).equals(map.get("code"))) {
-                UpdateWrapper<User> updateWrapper = new UpdateWrapper();
-                updateWrapper.eq("id", userID).set("phone_number", map.get("phoneNumber"));
-                boolean update = userService.update(updateWrapper);
-                if (!update) {
-                    return "-3";
-                }
-                // 认证成功 下发jwt
-                Map<String, Object> claims = new HashMap<>();
-                claims.put("userID", userID);
-                return JwtUtils.generateJwt(claims, 30 * 60L); // 30分钟
+            boolean update = userService.update(updateWrapper);
+            if (!update) {
+                return "-3";
             }
-        } else if (map.containsKey("email")) {
-            if (redisService.get(map.get("email").toString()) == null) {
-                return "-1";
-            }
-            if (redisService.get(map.get("email").toString()).equals(map.get("code"))) {
-                UpdateWrapper<User> updateWrapper = new UpdateWrapper();
-                updateWrapper.eq("id", userID).set("email", map.get("email"));
-                boolean update = userService.update(updateWrapper);
-                if (!update) {
-                    return "-3";
-                }
-                // 认证成功 下发jwt
-                Map<String, Object> claims = new HashMap<>();
-                claims.put("userID", userID);
-                return JwtUtils.generateJwt(claims, 30 * 60L);
-            }
+            // 认证成功 下发jwt
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("userID", userID);
+            return JwtUtils.generateJwt(claims);
+        } else {
+            return "-2";
         }
-        return "-2";
     }
 }
