f7508da71459dabd33349961e983697183d4fcfc
hu1hu <3261801992@qq.com>
2024-06-14 00:54:26 +0800

    style(server): 添加注释

--- server/src/main/java/org/pi/server/service/AuthCodeService.java
+++ server/src/main/java/org/pi/server/service/AuthCodeService.java
@@ -2,9 +2,12 @@ package org.pi.server.service;
 
 import java.util.Map;
 
+/**
+ * @author hu1hu
+ */
 public interface AuthCodeService {
     String loginOrRegisterAuthCode(Map<String,Object> map);
 
     String resetPasswordAuthCode(Map<String,Object> map);
-    String bind(String userID, Map<String, Object> requestMap);
+    String bind(String userID, Map<String, Object> map);
 }
