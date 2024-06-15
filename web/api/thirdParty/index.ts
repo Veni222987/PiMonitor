62e55b06643056c3acdae244687219d446033253
EddieHuang0712 <1826942933@qq.com>
2024-06-15 16:51:31 +0800

    feat(web): 定义第三方登录相关接口

---
+++ web/api/thirdParty/index.ts
@@ -0,0 +1,39 @@
+import {get} from "@/utils/APIUtils";
+import {authDel, authGet} from "@/utils/AuthUtils";
+
+/**
+ * 第三方登录
+ * @param {string} type
+ */
+export const ThirdPartyLogin = get<{
+    type: string
+}, {
+    redirectURL: string
+}>(`/v1/oauth/login/:type`)
+
+/**
+ * 绑定第三方账号
+ * @param {string} type
+ */
+export const BindThirdParty = authGet<{
+    type: string
+}, {
+    redirectURL: string
+}>(`/v1/oauth/bind/:type`)
+
+/**
+ * 第三方接触绑定
+ * @param {string} type
+ */
+export const UnbindThirdParty = authDel<{
+    type: string
+}, {}>(`/v1/oauth/unbind/:type`)
+
+// /**
+//  * 第三方授权回调
+//  * @param {string} redirectURL
+//  */
+// export const ThirdPartyCallback = request({
+//     url: `/v1/oauth/callback`,
+//     method: 'GET',
+// })
