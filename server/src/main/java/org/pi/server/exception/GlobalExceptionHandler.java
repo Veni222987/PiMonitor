83bab824aba23309e590607ef89fe3f674f663bf
hu1hu <3261801992@qq.com>
2024-06-24 22:10:51 +0800

    feat(server): 处理 @NotNull 异常

--- server/src/main/java/org/pi/server/exception/GlobalExceptionHandler.java
+++ server/src/main/java/org/pi/server/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.pi.server.common.Result;
 import org.pi.server.common.ResultCode;
 import org.pi.server.common.ResultUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,8 +17,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    /**
+     * 运行时异常
+     */
     @ExceptionHandler(RuntimeException.class)
     public Result<?> runtimeExceptionHandler(RuntimeException e) {
+        if (e instanceof IllegalArgumentException) {
+            return ResultUtils.error(ResultCode.PARAMS_ERROR);
+        }
         log.error("RuntimeException", e);
         return ResultUtils.error(ResultCode.SYSTEM_ERROR);
     }
