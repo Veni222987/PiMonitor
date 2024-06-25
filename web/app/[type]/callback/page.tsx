b9eb8bbdbf7a0d8896f159e7c91605c0a671fbfe
EddieHuang0712 <1826942933@qq.com>
2024-06-26 00:52:53 +0800

    feat(web): 搭建第三方授权回调页面

---
+++ web/app/[type]/callback/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {useParams} from "next/navigation";
+import {useEffect} from "react";
+import {setLocalStorage} from "@/utils/StorageUtils";
+
+export default function CallbackPage() {
+
+    useEffect(() => {
+        // 使用原生方法从?code=xxx中获取code
+        function getUrlParameter(name: string) {
+            name = name.replace(/[[]/, "\$$ ").replace(/[ $$]/, "\\]");
+            let regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+            let results = regex.exec(window.location.search);
+            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+
+        // 使用示例
+        let code = getUrlParameter('code');
+
+        // 将code存到Localstorage中
+        setLocalStorage('authCode', code);
+    }, []);
+
+    return (
+        <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-2xl">正在校验第三方授权...</h1>
+        </div>
+    );
+}
