a3094ce34d11a23bbded4b5b3404b22429218d38
EddieHuang0712 <1826942933@qq.com>
2024-06-28 00:21:31 +0800

    feat(web): 调整HeaderNav字体大小

--- web/components/HeaderNav.tsx
+++ web/components/HeaderNav.tsx
@@ -9,12 +9,11 @@ const HeaderNav: React.FC<ChildrenProps> = (props) => {
         // left
         <div className="fixed w-full h-10vh flex justify-between items-center px-6 py-4 z-10">
             <div className="flex items-center hover:scale-105">
-                <div className="h-full text-white font-bold font-franklin text-2xl italic">
+                <div className="h-full text-white font-bold font-franklin text-4xl italic">
                     <a href='/'>PiMonitor</a>
                 </div>
             </div>
 
-            {/* right */}
             <div className="flex justify-center items-center">
                 <div className="rounded-full transition-transform transform hover:scale-105">
                     <div className='url'>
@@ -28,25 +27,6 @@ const HeaderNav: React.FC<ChildrenProps> = (props) => {
                         </a>
                     </div>
                 </div>
-                {/*{!ifHideUser && (*/}
-                {/*    <div className="flex items-center justify-center leading-9">*/}
-                {/*        <div className="leading-9">*/}
-                {/*            <Link*/}
-                {/*                className="block transition-transform transform hover:scale-105"*/}
-                {/*                href='/'>*/}
-                {/*                <Avatar*/}
-                {/*                    size={40}*/}
-                {/*                    style={{ backgroundColor: 'black', marginLeft: '10px' }}*/}
-                {/*                    icon={!user_info?.avatar_url && <UserOutlined />}*/}
-                {/*                    src={user_info?.avatar_url}*/}
-                {/*                    alt='用户头像'*/}
-                {/*                />*/}
-                {/*            </Link>*/}
-                {/*        </div>*/}
-
-                {/*        <div className="text-center text-indigo-600 ml-4">{user_info?.username || null}</div>*/}
-                {/*    </div>*/}
-                {/*)}*/}
             </div>
         </div>
     )
